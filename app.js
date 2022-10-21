d428cdcb022e683bdcd4aae9e71758ad045f78f8
goncalogbastos <bastos.g.goncalo@gmail.com>
2022-10-21 19:59:41 +0100

    Mapbox feature

--- app.js
+++ app.js
@@ -1,119 +1,179 @@
-	// TO MAKE THE MAP APPEAR YOU MUST
-	// ADD YOUR ACCESS TOKEN FROM
-	// https://account.mapbox.com
-	mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+import { Camera, Scene, DirectionalLight, Vector3, Matrix4, WebGLRenderer } from 'three';
+
+import {IFCLoader} from 'web-ifc-three';
+
+
+const coordinates = [-9.260586787894955,39.091593392308724];
+
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken = 'ACCESS_TOKEN';
 const map = new mapboxgl.Map({
-container: 'map',
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-style: 'mapbox://styles/mapbox/light-v10',
-zoom: 18,
-center: [148.9819, -35.3981],
-pitch: 60,
-antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 18,
+    center: coordinates,
+    pitch: 60,
+    antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
 });
- 
+
 // parameters to ensure the model is georeferenced correctly on the map
-const modelOrigin = [148.9819, -35.39847];
+const modelOrigin = coordinates;
 const modelAltitude = 0;
 const modelRotate = [Math.PI / 2, 0, 0];
- 
+
 const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
-modelOrigin,
-modelAltitude
+    modelOrigin,
+    modelAltitude
 );
- 
+
 // transformation parameters to position, rotate and scale the 3D model onto the map
 const modelTransform = {
-translateX: modelAsMercatorCoordinate.x,
-translateY: modelAsMercatorCoordinate.y,
-translateZ: modelAsMercatorCoordinate.z,
-rotateX: modelRotate[0],
-rotateY: modelRotate[1],
-rotateZ: modelRotate[2],
-/* Since the 3D model is in real world meters, a scale transform needs to be
-* applied since the CustomLayerInterface expects units in MercatorCoordinates.
-*/
-scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+    translateX: modelAsMercatorCoordinate.x,
+    translateY: modelAsMercatorCoordinate.y,
+    translateZ: modelAsMercatorCoordinate.z,
+    rotateX: modelRotate[0],
+    rotateY: modelRotate[1],
+    rotateZ: modelRotate[2],
+    /* Since the 3D model is in real world meters, a scale transform needs to be
+    * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+    */
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
 };
- 
+
 const THREE = window.THREE;
- 
+
 // configuration of the custom layer for a 3D model per the CustomLayerInterface
 const customLayer = {
-id: '3d-model',
-type: 'custom',
-renderingMode: '3d',
-onAdd: function (map, gl) {
-this.camera = new THREE.Camera();
-this.scene = new THREE.Scene();
- 
-// create two three.js lights to illuminate the model
-const directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(0, -70, 100).normalize();
-this.scene.add(directionalLight);
- 
-const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-directionalLight2.position.set(0, 70, 100).normalize();
-this.scene.add(directionalLight2);
- 
-// use the three.js GLTF loader to add the 3D model to the three.js scene
-const loader = new THREE.GLTFLoader();
-loader.load(
-'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
-(gltf) => {
-this.scene.add(gltf.scene);
-}
-);
-this.map = map;
- 
-// use the Mapbox GL JS map canvas for three.js
-this.renderer = new THREE.WebGLRenderer({
-canvas: map.getCanvas(),
-context: gl,
-antialias: true
-});
- 
-this.renderer.autoClear = false;
-},
-render: function (gl, matrix) {
-const rotationX = new THREE.Matrix4().makeRotationAxis(
-new THREE.Vector3(1, 0, 0),
-modelTransform.rotateX
-);
-const rotationY = new THREE.Matrix4().makeRotationAxis(
-new THREE.Vector3(0, 1, 0),
-modelTransform.rotateY
-);
-const rotationZ = new THREE.Matrix4().makeRotationAxis(
-new THREE.Vector3(0, 0, 1),
-modelTransform.rotateZ
-);
- 
-const m = new THREE.Matrix4().fromArray(matrix);
-const l = new THREE.Matrix4()
-.makeTranslation(
-modelTransform.translateX,
-modelTransform.translateY,
-modelTransform.translateZ
-)
-.scale(
-new THREE.Vector3(
-modelTransform.scale,
--modelTransform.scale,
-modelTransform.scale
-)
-)
-.multiply(rotationX)
-.multiply(rotationY)
-.multiply(rotationZ);
- 
-this.camera.projectionMatrix = m.multiply(l);
-this.renderer.resetState();
-this.renderer.render(this.scene, this.camera);
-this.map.triggerRepaint();
-}
+    id: '3d-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function (map, gl) {
+        this.camera = new Camera();
+        this.scene = new Scene();
+
+        // Load IFC model
+        loadModel(this.scene);
+
+        // create two three.js lights to illuminate the model
+        const directionalLight = new DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        this.map = map;
+
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new WebGLRenderer({
+            canvas: map.getCanvas(),
+            context: gl,
+            antialias: true
+        });
+
+        this.renderer.autoClear = false;
+    },
+    render: function (gl, matrix) {
+        const rotationX = new Matrix4().makeRotationAxis(
+            new Vector3(1, 0, 0),
+            modelTransform.rotateX
+        );
+        const rotationY = new Matrix4().makeRotationAxis(
+            new Vector3(0, 1, 0),
+            modelTransform.rotateY
+        );
+        const rotationZ = new Matrix4().makeRotationAxis(
+            new Vector3(0, 0, 1),
+            modelTransform.rotateZ
+        );
+
+        const m = new Matrix4().fromArray(matrix);
+        const l = new Matrix4()
+            .makeTranslation(
+                modelTransform.translateX,
+                modelTransform.translateY,
+                modelTransform.translateZ
+            )
+            .scale(
+                new Vector3(
+                    modelTransform.scale,
+                    -modelTransform.scale,
+                    modelTransform.scale
+                )
+            )
+            .multiply(rotationX)
+            .multiply(rotationY)
+            .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+    }
 };
- 
+
 map.on('style.load', () => {
-map.addLayer(customLayer, 'waterway-label');
+    map.addLayer(customLayer, 'waterway-label');
 });
+
+
+map.on('load', () => {
+    // Insert the layer beneath any symbol layer.
+    const layers = map.getStyle().layers;
+    const labelLayerId = layers.find(
+        (layer) => layer.type === 'symbol' && layer.layout['text-field']
+    ).id;
+
+    // The 'building' layer in the Mapbox Streets
+    // vector tileset contains building height data
+    // from OpenStreetMap.
+    map.addLayer(
+        {
+            'id': 'add-3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // Use an 'interpolate' expression to
+                // add a smooth transition effect to
+                // the buildings as the user zooms in.
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+});
+
+
+async function loadModel(scene){
+    const loader = new IFCLoader();
+    await loader.ifcManager.setWasmPath("/wasm");
+    const model = await loader.load("01.ifc");
+    scene.add(model);
+}
